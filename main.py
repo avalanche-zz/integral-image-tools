51790ea4953126c5450db39c88a609b0652e5853
Andrey Nosov <hitotricorn@gmail.com>
2021-12-16 06:04:59 +0700

    Created main.py with templates for functions

---
+++ main.py
@@ -0,0 +1,10 @@
+def generate_matrix() -> list[list[int]]:
+    ...
+
+
+def integral_view(image: list[list[int]]) -> list[list[int]]:
+    ...
+
+
+def rect_sum(image: list[list[int]], x1: int, y1: int, x2: int, y2: int) -> int:
+    ...
